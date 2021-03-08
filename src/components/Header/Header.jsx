9aa7a2e4a9d857c69bdfdff84e9696c69de5ba28
Евгений <catshaurma@gmail.com>
2021-03-08 14:02:15 +0300

    61. auth/me implementation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,17 @@
 import c from "./Header.module.css";
+import {NavLink} from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
     return (
         <header className={c.header}>
             <img
                 src="https://png.pngtree.com/element_our/png/20181011/twitter-social-media-icon-design-template-vector-png_127095.jpg"
                 alt="logo"/>
+            <div className={c.loginBlock}>
+                {
+                    props.isAuth ? props.login : <NavLink to='/login'>Login</NavLink>
+                }
+            </div>
         </header>
     );
 }
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -2,11 +2,13 @@ import {combineReducers, createStore} from 'redux';
 import messageReducer from "./messageReducer";
 import profileReducer from "./profileReducer";
 import usersReducer from "./usersReducer";
+import authReducer from "./authReducer";
 
 let reducers = combineReducers({
     messagePage: messageReducer,
     profilePage: profileReducer,
-    usersPage: usersReducer
+    usersPage: usersReducer,
+    auth: authReducer
 });
 
 let store = createStore(reducers);
