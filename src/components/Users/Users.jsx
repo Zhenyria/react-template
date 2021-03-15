018a3cc6710be38f41c7e43deba2ccb99522116c
Евгений <catshaurma@gmail.com>
2021-03-15 23:47:39 +0300

    93. Update pagination

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,7 +4,7 @@ import User from "./User";
 const Users = ({currentPage, setCurrentPage, totalUsersCount, pageSize, ...props}) => {
     return (
         <div>
-            <Paginator currentPage={currentPage} setCurrentPage={setCurrentPage} totalUsersCount={totalUsersCount}
+            <Paginator currentPage={currentPage} setCurrentPage={setCurrentPage} totalItemsCount={totalUsersCount}
                        pageSize={pageSize}/>
             {props.users.map(u => <User user={u} isFollowingInProgress={props.isFollowingInProgress}
                                         follow={props.follow} unfollow={props.unfollow}/>)}
--- src/components/common/Paginator/Paginator.jsx
+++ src/components/common/Paginator/Paginator.jsx
@@ -1,19 +1,42 @@
 import c from './Paginator.module.css';
 
-const Paginator = (props) => {
-    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+const Paginator = ({totalItemsCount, pageSize, currentPage, setCurrentPage, portionSize = 10}) => {
+    let pagesCount = Math.ceil(totalItemsCount / pageSize);
     let pages = [];
     for (let i = 0; i < pagesCount; i++) {
         pages.push(i + 1);
     }
 
+    let leftPortionPageNumber = currentPage > 7 ? currentPage - 5 : 1;
+    let rightPortionPageNumber = currentPage + 6 < pagesCount ? currentPage + 5 : totalItemsCount;
+
+    let isFirstPageMustBe = leftPortionPageNumber > 1;
+    let isLastPageMustBe = rightPortionPageNumber < totalItemsCount;
+
+    let isLeftMediumPointMustBe = leftPortionPageNumber - 1 > 0;
+    let isRightMediumPointMustBe = pagesCount - rightPortionPageNumber > 0;
+
     return (
         <div>
-            {pages.map(pageNum => {
-                return <span onClick={() => props.setCurrentPage(pageNum)}
-                             className={(props.currentPage === pageNum && c.selected) + ' ' + c.paginationItem}>
+            {isFirstPageMustBe && <span onClick={() => setCurrentPage(1)} className={c.paginationItem}>1</span>}
+
+            {isLeftMediumPointMustBe && <span onClick={() => setCurrentPage(Math.ceil((leftPortionPageNumber + 1) / 2))}
+                                              className={c.paginationItem}>...</span>}
+
+            {pages
+                .filter(pageNum => pageNum >= leftPortionPageNumber && pageNum <= rightPortionPageNumber)
+                .map(pageNum => {
+                    return <span onClick={() => setCurrentPage(pageNum)}
+                                 className={(currentPage === pageNum && c.selected) + ' ' + c.paginationItem}>
                                 {pageNum}</span>
-            })}
+                })}
+
+            {isRightMediumPointMustBe &&
+            <span onClick={() => setCurrentPage(Math.ceil((pagesCount + rightPortionPageNumber) / 2))}
+                  className={c.paginationItem}>...</span>}
+
+            {isLastPageMustBe &&
+            <span onClick={() => setCurrentPage(pagesCount)} className={c.paginationItem}>{pagesCount}</span>}
         </div>
     )
 }
